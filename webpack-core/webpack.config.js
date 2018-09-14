1cc05e45fdaf718aa990bcc91c2a724b50167f24
lv-conner <lvmh123456@gmail.com>
2018-09-14 17:11:03 +0800

    添加项目文件。

---
+++ webpack-core/webpack.config.js
@@ -0,0 +1,47 @@
+﻿const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+    entry: {
+        "home": "./static/index.js",
+        "hello": "./static/hello.js"
+    },
+    output: {
+        path: path.join(__dirname, "./wwwroot/static/dist"),
+        filename: "[name]-bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['./wwwroot/static/dist']),
+        new HtmlWebpackPlugin({
+            template: "./static/index.html",
+            filename: "../../../Views/Home/home.cshtml",
+            chunks: ['commons', 'home'],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./static/hello.html",
+            filename: "hello.cshtml",
+            chunks: ['commons', "hello"]
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                // 创建一个 commons 块，用于包含所有入口模块共用的代码
+                commons: {
+                    name: "commons",
+                    chunks: "initial",
+                    minChunks: 2
+                }
+            }
+        }
+    }
+}
